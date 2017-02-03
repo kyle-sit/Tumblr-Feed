b225f0f987e221d5baa2c5be14f86c1c5c55f08b
Kyle Sit <Kyle@kyles-macbook-pro-4.local>
2017-02-02 21:07:06 -0800

    Photo feed!!!!~!~

---
+++ Tumblr Feed/PhotosViewController.swift
@@ -0,0 +1,99 @@
+//
+//  PhotosViewController.swift
+//  Tumblr Feed
+//
+//  Created by Kyle Sit on 2/2/17.
+//  Copyright © 2017 Kyle Sit. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var posts: [NSDictionary]? = [];
+    @IBOutlet weak var TumblrTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TumblrTable.delegate = self
+        TumblrTable.dataSource = self
+        TumblrTable.rowHeight = 240
+        
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        
+                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                        
+                        self.TumblrTable.reloadData()
+                        
+                        // This is where you will store the returned array of posts in your posts property
+                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                    }
+                }
+        });
+        task.resume()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let posts = posts {
+            return posts.count
+        }
+        else {
+            return 0
+        }
+    }
+    
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = TumblrTable.dequeueReusableCell(withIdentifier: "TumblrCell", for: indexPath) as! TumblrCell
+        let post = posts?[indexPath.row]
+        if let photos = post?.value(forKeyPath: "photos") as? [NSDictionary] {
+            let imageUrlString = photos[0].value(forKeyPath: "original_size.url") as? String
+            if let imageUrl = URL(string: imageUrlString!) {
+                cell.photoImage.setImageWith(imageUrl)
+            } else {
+            }
+        } else {
+        }
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tumblr Feed/TumblrCell.swift
@@ -0,0 +1,26 @@
+//
+//  TumblrCell.swift
+//  Tumblr Feed
+//
+//  Created by Kyle Sit on 2/2/17.
+//  Copyright © 2017 Kyle Sit. All rights reserved.
+//
+
+import UIKit
+
+class TumblrCell: UITableViewCell {
+
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
